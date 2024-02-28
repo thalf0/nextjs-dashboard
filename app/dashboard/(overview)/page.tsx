097362fa16262ed591e62fe83602949d08b9ef2e
thalf0 <thealfodev@outlook.com>
2024-02-28 17:59:21 +0800

    streaming skeleton suspence fetch revenue

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,11 +2,13 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data'; // remove fetchRevenue
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 // async allows to use await to fetch data
 export default async function Page() {
-  const revenue = await fetchRevenue();
+  // const revenue = await fetchRevenue(); 
   const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfCustomers,
@@ -27,7 +29,9 @@ export default async function Page() {
         <Card title="Total Customers" value={numberOfCustomers} type="customers" />
       </div>
       <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
-        <RevenueChart revenue={revenue} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
